0e2abe9f1650535247895052b911ddbb7a407856
ayush0x1 <ayush7code@gmail.com>
2026-01-12 13:51:27 +0530

    main
    main file

---
+++ install.py
@@ -0,0 +1,75 @@
+from time import sleep
+from os import system as sys
+
+
+class requre:
+     req="apt update && apt install x11-repo"
+     ins="apt install polybar openbox tigervnc feh jp2a xcompmgr firefox pcmanfm  zip git rofi -y"
+
+
+def courser():
+     sys('mkdir -p ~/.icons ~/.icons/default ~/tmp && curl -L https://archive.org/download/win-sur-white-cursors.tar/WinSur-white-cursors.tar.gz -o ~/tmp/winsur.tar.gz && tar -xzf ~/tmp/winsur.tar.gz -C ~/.icons && rm ~/tmp/winsur.tar.gz && printf "[Icon Theme]\nInherits=WinSur-white-cursors\n" > ~/.icons/default/index.theme && printf "export XCURSOR_THEME=WinSur-white-cursors\nexport XCURSOR_SIZE=24\n" > ~/.xprofile')
+
+
+def theme():
+     sys("wget https://archive.org/download/themes_202601/themes.zip -O ~/../usr/share/themes/themes.zip && unzip ~/../usr/share/themes/themes.zip ")
+
+def wall():
+     from colorama import Fore, Back, Style, init
+     init(autoreset=True)
+
+     sys("clear")
+     print(Style.BRIGHT + Fore.GREEN + "\n[+] Polybar configuring")
+     sleep(3)
+     sys("mkdir ~/.config/polybar")
+     sys("cd ~/.config/polybar")
+     sys("wget https://archive.org/download/polybar/polybar.zip && unzip polybar.zip && rm polybar.zip")
+     sys("cd polybar-themes && bash setup.sh && rm -rf polybar-themes")
+     sys("mkdir ~/Picture && mkdir Downloads && mkdir Public && cd ~/")
+     print("wallpaper")
+     sys("rm Picture/*")
+     sys("wget https://wallpaper-clan.com/wp-content/uploads/2025/12/spy-x-family-anya-forger-hot-cocoa-lamppost-desktop-wallpaper-preview.jpg -O ~/Picture/wall.jpg ")
+
+def vnc():
+      from colorama import Fore, Back, Style, init
+      init(autoreset=True)
+
+      sys("clear")
+      print(Fore.BLUE + "[+] Tigervnc ")
+      sleep(3)
+      sys("vncserver")
+      con = """\nexport DISPLAY=:1\nsed -i  "s|<name>.*</name>|<name>Prismatic-Night</name>|g" ~/.config/openbox/rc.xml\nopenbox --reconfigure &\nxcompmgr &\nfeh --bg-fill ~/Picture/wall.jpg &\n(sleep 2 && ~/.config/polybar/docky/launch.sh &>/dev/null &) &\nexec openbox-session\n """
+      with open('.vnc/xstartup', 'w') as file:
+         file.write(con)
+      print("[+] Done")
+
+      text="pkill Xvnc\nvncserver :1"
+      with open('start-desktop', 'w') as file:
+         file.write(text)
+      sys("pkill Xvnc")
+      sys("chmod +x start-desktop && chmod +x .vnc/xstartup && clear && mv start-desktop ~/../usr/bin && start-desktop")
+      print(Style.BRIGHT + Fore.GREEN + "[+] Next time use command : start-desktop")
+      
+
+def main():
+     from colorama import Fore, Back, Style, init
+
+     sys("jp2a https://assets.stickpng.com/images/613098fd48f1e30004910189.png  --color --color ")
+     print(Style.BRIGHT + Fore.BLUE + "Created by: ayush \n")
+     sleep(2)
+     sys(requre.req)
+     sys(requre.ins)
+     print("\n[+] Instaled")
+     sleep(2)
+
+try:
+   main()
+   wall()
+   courser()
+   vnc()
+        
+except ImportError:
+   print("[!] Installing : pip install colorama==0.4.6 \n")
+   sys("pip install colorama==0.4.6")
+   sys("python install.py")
+
